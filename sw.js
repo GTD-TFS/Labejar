7b86996f0b507dba1d311961350bbf77c9a43773
GTD-TFS <adeje.gtd@hotmail.com>
2025-12-22 13:39:58 +0000

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,1 +1,32 @@
-self.addEventListener('fetch', () => {});
+const CACHE = 'labejar-v1';
+const ASSETS = [
+  '/Labejar/',
+  '/Labejar/index.html',
+  '/Labejar/Fil.cleaned.html',
+  '/Labejar/manifest.webmanifest',
+  '/Labejar/abeja.png',
+  '/Labejar/parser.js',
+  '/Labejar/municipios.js',
+  '/Labejar/provincias_es.js',
+  '/Labejar/paises.js',
+  '/Labejar/calles.js',
+  '/Labejar/icons/icon-192.png',
+  '/Labejar/icons/icon-512.png',
+  '/Labejar/icons/icon-192-maskable.png',
+  '/Labejar/icons/icon-512-maskable.png'
+];
+
+self.addEventListener('install', (e) => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (e) => {
+  e.waitUntil(self.clients.claim());
+});
+
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    caches.match(e.request).then(r => r || fetch(e.request))
+  );
+});
